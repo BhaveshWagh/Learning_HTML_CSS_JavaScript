f85fcbec6e04b98ff285a710bffcb000003dc83a
BhaveshWagh <bhaveshbm2711@gmail.com>
2023-02-13 12:50:42 +0530

    uncomment the code

--- Learn_javaScript/problem.js
+++ Learn_javaScript/problem.js
@@ -1,23 +1,23 @@
-// function x() {
-//     var i = 10;
-//     setTimeout(function () {
-//         console.log(i);
-//     },3000);
-//     console.log("Namaste JavaScript");    
-// }
-// x();
+function x() {
+    var i = 10;
+    setTimeout(function () {
+        console.log(i);
+    },3000);
+    console.log("Namaste JavaScript");    
+}
+x();
 
 // closures example 
-// var sum = function(a){
-//     console.log("Hello, Hello your rating is " + a + "/5")
-//     var c = 2
-//     return function(b){
-//         console.log("This is a return function.The value of b =",b)
-//         return a+b+c
-//     }
-// }
-// var ret = sum(3)
-// console.log(ret(5));
+var sum = function(a){
+    console.log("Hello, Hello your rating is " + a + "/5")
+    var c = 2
+    return function(b){
+        console.log("This is a return function.The value of b =",b)
+        return a+b+c
+    }
+}
+var ret = sum(3)
+console.log(ret(5));
 
 var sum = function (a,b,c) {
     console.log("this outer block");
