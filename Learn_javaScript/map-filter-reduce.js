f5a08a8df8a5efe35415ab18c296651747bc2a58
BhaveshWagh <bhaveshbm2711@gmail.com>
2023-02-18 23:20:03 +0530

    learned Higher Order Function

---
+++ Learn_javaScript/map-filter-reduce.js
@@ -0,0 +1,7 @@
+// map() function javascript: The map() method creates a new array populated with the results of calling a provided function on every element in the calling array.
+// eg: 
+
+const arr = [1,2,3,4]
+
+const 
+
