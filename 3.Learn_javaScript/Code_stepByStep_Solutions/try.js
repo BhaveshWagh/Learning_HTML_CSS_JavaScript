4a6c3b35aaf4dc50c7fa79ebadda37f0b0579918
BhaveshWagh <bhaveshbm2711@gmail.com>
2023-03-15 18:30:05 +0530

    problem solved

--- 3.Learn_javaScript/Code_stepByStep_Solutions/try.js
+++ 3.Learn_javaScript/Code_stepByStep_Solutions/try.js
@@ -1,12 +1,55 @@
-// This code is not complete half part of solution is done half part is remaining
-result = '';
-for (let i = 1; i <= 7; i++){
-    for (j = 1; j <= i; j++){
-        result += i;
+// var twoSum = function(nums, target) {
+//     let sum = 0;
+//     for(let i = 0; i < nums.length; i++){
+//         sum = nums[i] + nums[i + 1]
+//         // console.log(sum)
+//         if (target == sum){
+//         }
+//     } 
+    
+// };
+
+// var twoSum = function(nums, target) {
+//     var map = {};
+//     // console.log(typeof(map))
+//     for(var i = 0 ; i < nums.length ; i++){
+//         var n = nums[i];
+//         // console.log(n)
+//         if(map[target-n] >= 0){
+//             // console.log([map[target-n],i])
+//             return [map[target-n],i]
+//         }
+//         else {
+//             map[n] = i;   //use map to store the value and index position
+//         }
+//     }
+//     // console.log(map)
+// };
+
+var twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        let firstNumber = nums[i];
+        for (let j = 0; j < nums.length; j++) {
+            let secondNumber = nums[j];
+            if (firstNumber + secondNumber === target && i !== j) {
+                console.log([i, j]) 
+            }
+        }
     }
-    result += '\n';
-}
-console.log(result)
+};
+twoSum([1,2,4,5],9)
+
+
+
+// This code is not complete half part of solution is done half part is remaining
+// result = '';
+// for (let i = 1; i <= 7; i++){
+//     for (j = 1; j <= i; j++){
+//         result += i;
+//     }
+//     result += '\n';
+// }
+// console.log(result)
 
 // result = '';
 // for (let i = 5; i <= 10; i++){
