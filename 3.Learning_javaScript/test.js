b6c64dd46da85596b114233037f8fa8b196195d9
BhaveshWagh <bhaveshbm2711@gmail.com>
2023-04-25 22:47:52 +0530

    removeDuplicate code

---
+++ 3.Learning_javaScript/test.js
@@ -0,0 +1,19 @@
+function removeDuplicates(array) {
+  let uniqueArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    console.log("****",array[i])
+    if (uniqueArray.indexOf(array[i]) === -1) {
+      console.log(array[i]);
+      uniqueArray.push(array[i]);
+    }
+  }
+
+  return uniqueArray;
+}
+
+let myArray = [1, 2, 3, 2, 4, 3, 5];
+let uniqueArray = removeDuplicates(myArray);
+
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
+console.log(myArray.indexOf());
