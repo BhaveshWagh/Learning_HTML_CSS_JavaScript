a022eec6015970c013338d92d1ebd82d50327fe0
BhaveshWagh <bhaveshbm2711@gmail.com>
2023-06-15 13:18:07 +0530

    brush up js solving problems

---
+++ 3.Learning_javaScript/ProblemSolved/3.lengthConversion.js
@@ -0,0 +1,11 @@
+// 3. Create a length converter function
+//  1 Km in 1 Miles is 0.621371 
+// 1 km  = 0.621371 miles
+// 1 miles = 1.609343 km
+function lengthConversion(km){
+
+    let KmToMiles = km * 0.621371;
+    console.log(KmToMiles)
+}
+
+lengthConversion(5)
