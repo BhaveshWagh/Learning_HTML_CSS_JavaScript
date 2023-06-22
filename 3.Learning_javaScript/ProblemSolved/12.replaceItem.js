363aecf1c38134135a5564836dd14a0696ed741e
BhaveshWagh <bhaveshbm2711@gmail.com>
2023-06-22 11:08:58 +0530

    replace item problem solved

---
+++ 3.Learning_javaScript/ProblemSolved/12.replaceItem.js
@@ -0,0 +1,12 @@
+// How to replace item in any array:
+
+let fruits = ["apple", "grape", "mango"];
+
+// find the index of the item to replace
+let index = fruits.indexOf("grape");
+
+// Replace the item at the found index
+
+fruits[index] = "orange"
+
+console.log(fruits)
